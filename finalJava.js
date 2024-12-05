930731a843b82ca2ad237d6a437339b852b1dbd5
Jaida Ries <jaida.ries@gmail.com>
2024-12-05 10:37:13 -0500

    Ticketmaster API fetch and add dots to map

--- finalJava.js
+++ finalJava.js
@@ -8,6 +8,37 @@ function newMap(){
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    fetchEvents(map);
+}
+
+/* Fetch Events from Ticketmaster API */ 
+function fetchEvents(map) {
+    const apiKey = "MJqQ6NUkUDHFon1F2xTypEX2NS7FGlGn"; //do we need to hide this later 
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&size=10&apikey=${apiKey}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if(data._embedded && data._embedded.events) {
+                const events = data._embedded.events;
+                events.forEach(event => {
+                    const city = event._embedded.venues[0].city.name;
+                    const lat = event._embedded.venues[0].location.latitude;
+                    const lng = event._embedded.venues[0].location.longitude;
+
+                    //Add markers for each event 
+                    const marker = L.marker([lat, lng]).addTo(map);
+                    marker.bindPopup(`
+                        <strong>${event.name}</strong><br>
+                        ${city}<br>
+                        <a href="${event.url}" target="_blank">More Info</a>
+                    `);
+                })
+            }
+        })
 }
 
 /* Load header and footer on every page */
